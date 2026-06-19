35de2f0af721464e416296ef4237cd1a1e077c2e
JahanzaibDev10 <jahanzaibc03@gmail.com>
2026-06-19 22:35:35 +0500

    Did some vercel changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 import path from "node:path";
 
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
         quoteStyle: "double",
       },
     }),
+    nitro(),
     tailwindcss(),
     react(),
   ],
